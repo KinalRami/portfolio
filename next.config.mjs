7fe2aedef88a058e4fb6c359b48a9ced30242971
kinalrami <ramikinal0501@gmail.com>
2025-06-27 22:44:58 +0530

    Fixed design issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-// next.config.mjs
-const isProd = process.env.NODE_ENV === 'production';
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-export default {
-  output: 'export',
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '',
-};
+export default nextConfig;
